4ee8b5829bf76da3044c4e020197323b36060a69
Christian <christian.medina@isobar.com>
2024-02-12 00:42:38 -0500

    contacto y ajuste varios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Loading
+
 const loading = setInterval(() => {
     if (document.readyState === "complete" || document.readyState === "interactive") {
         document.body.classList.add("loading");
@@ -7,7 +9,25 @@ const loading = setInterval(() => {
     }
 }, 500);
 
+// Scroll Suave
 
-// loading()
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+
 
 
